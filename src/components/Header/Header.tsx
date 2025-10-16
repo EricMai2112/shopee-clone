e318aa6c1b5570b40dadba19c50850f9c452e398
EricMai2112 <mait58674@gmail.com>
2025-10-16 23:57:51 +0700

    done a tooltip for language

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,13 @@
 import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { arrow, FloatingPortal, useFloating } from '@floating-ui/react-dom-interactions'
+import { arrow, FloatingPortal, offset, shift, useFloating } from '@floating-ui/react-dom-interactions'
+import { motion, AnimatePresence } from 'framer-motion'
 
 export default function Header() {
   const [open, setOpen] = useState(true)
   const arrowRef = useRef<HTMLElement>(null)
   const { x, y, reference, floating, strategy, middlewareData } = useFloating({
-    middleware: [arrow({ element: arrowRef })]
+    middleware: [offset(5), shift(), arrow({ element: arrowRef })]
   })
   const showPopover = () => {
     setOpen(true)
@@ -40,7 +41,7 @@ export default function Header() {
                 d='M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418'
               />
             </svg>
-            <span className='mx-1'>Tiếng Việt</span>
+            <span className='mx-1 '>Tiếng Việt</span>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
@@ -51,27 +52,42 @@ export default function Header() {
             >
               <path strokeLinecap='round' strokeLinejoin='round' d='m19.5 8.25-7.5 7.5-7.5-7.5' />
             </svg>
+            <FloatingPortal>
+              <AnimatePresence>
+                {open && (
+                  <motion.div
+                    ref={floating}
+                    style={{
+                      position: strategy,
+                      top: y ?? 0,
+                      left: x ?? 0,
+                      width: 'max-content',
+                      transformOrigin: `${middlewareData.arrow?.x}px top`
+                    }}
+                    initial={{ opacity: 0, transform: 'scale(0)' }}
+                    animate={{ opacity: 1, transform: 'scale(1)' }}
+                    exit={{ opacity: 0, transform: 'scale(0)' }}
+                    transition={{ duration: 0.2 }}
+                  >
+                    <span
+                      ref={arrowRef}
+                      className='border-x-transparent border-t-transparent border-b-white border-[11px] absolute -translate-y-full'
+                      style={{
+                        left: middlewareData.arrow?.x,
+                        top: middlewareData.arrow?.y
+                      }}
+                    ></span>
+                    <div className='bg-white relative shadow-md rounded-sm border border-gray-200'>
+                      <div className='flex flex-col py-2 px-3'>
+                        <button className='py-2 px-3 hover:text-[#ee4d2d] cursor-pointer'>Tiếng Việt</button>
+                        <button className='py-2 px-3 hover:text-[#ee4d2d] cursor-pointer'>Tiếng Anh</button>
+                      </div>
+                    </div>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </FloatingPortal>
           </div>
-          <FloatingPortal>
-            {open && (
-              <div ref={floating} style={{ position: strategy, top: y ?? 0, left: x ?? 0, width: 'max-content' }}>
-                <span
-                  ref={arrowRef}
-                  className='border-x-transparent border-t-transparent border-b-white border-[11px] absolute -translate-y-full'
-                  style={{
-                    left: middlewareData.arrow?.x,
-                    top: middlewareData.arrow?.y
-                  }}
-                ></span>
-                <div className='bg-white relative shadow-md rounded-sm border border-gray-200'>
-                  <div className='flex flex-col py-2 px-3'>
-                    <button className='py-2 px-3 hover:text-[#ee4d2d]'>Tiếng Việt</button>
-                    <button className='py-2 px-3 hover:text-[#ee4d2d]'>Tiếng Anh</button>
-                  </div>
-                </div>
-              </div>
-            )}
-          </FloatingPortal>
 
           <div className='flex items-center py-1 hover:text-gray-300 cursor-pointer ml-6'>
             <div className='w-6 h-6 mr-2 flex-shrink-0'>
