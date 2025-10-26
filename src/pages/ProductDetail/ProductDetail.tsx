9fe43d28f0c82988ec07f5b174b13a596ce122df
EricMai2112 <mait58674@gmail.com>
2025-10-27 00:02:12 +0700

    display images slider

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -1,10 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
-import React from 'react'
+import DOMPurify from 'dompurify'
+import React, { useEffect, useMemo, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import productApi from 'src/apis/product.api'
-import RatingStars from '../ProductList/components/RatingStars'
+import InputNumber from 'src/components/InputNumber'
 import ProductRating from 'src/components/ProductRating'
-import { formartNumberToSocialStyle, formatCurrency } from 'src/utils/utils'
+import { formartNumberToSocialStyle, formatCurrency, rateSale } from 'src/utils/utils'
 
 export default function ProductDetail() {
   const { id } = useParams()
@@ -12,19 +13,31 @@ export default function ProductDetail() {
     queryKey: ['product', id],
     queryFn: () => productApi.getProductDetail(id as string)
   })
+
   const product = productDetailData?.data.data
+  const [currentIndexImage, setCurrentIndexImage] = useState([0, 5])
+  const [activeImage, setActiveImage] = useState('')
+
+  useEffect(() => {
+    if (product && product.images.length > 0) {
+      setActiveImage(product.images[0])
+    }
+  }, [product])
+
+  const currentImages = useMemo(() => (product ? product?.images.slice(...currentIndexImage) : []), [product])
+
   console.log(product)
   if (!product) return null
 
   return (
     <div className='bg-gray-200 py-6'>
-      <div className='bg-white p-4 shadow'>
-        <div className='max-w-7xl mx-auto px-4'>
+      <div className='max-w-7xl mx-auto px-4'>
+        <div className='bg-white p-4 shadow'>
           <div className='grid grid-cols-12 gap-9'>
             <div className='col-span-5'>
               <div className='relative w-full pt-[100%] shadow'>
                 <img
-                  src={product.image}
+                  src={activeImage}
                   alt={product.name}
                   className='absolute top-0 left-0 bg-white w-full h-full object-cover'
                 />
@@ -42,13 +55,13 @@ export default function ProductDetail() {
                     <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5 8.25 12l7.5-7.5' />
                   </svg>
                 </button>
-                {product.images.slice(0, 5).map((img, index) => {
+                {currentImages.slice(0, 5).map((img, index) => {
                   const isActive = index == 0
 
                   return (
                     <div className='relative w-full pt-[100%]' key={img}>
                       <img
-                        src={product.image}
+                        src={img}
                         alt={product.name}
                         className='absolute top-0 left-0 bg-white cursor-pointer w-full h-full object-cover'
                       />
@@ -90,8 +103,81 @@ export default function ProductDetail() {
               <div className='mt-8 flex items-center bg-gray-50 px-5 py-4'>
                 <div className='text-gray-500 line-through'>₫{formatCurrency(product.price_before_discount)}</div>
                 <div className='ml-3 text-3xl font-medium text-[#ee4d2d]'>₫{formatCurrency(product.price)}</div>
+                <div className='ml-4 rounded-sm bg-[#ee4d2d] px-1 py-2 text-xs font-semibold uppercase text-white'>
+                  {rateSale(product.price_before_discount, product.price)} giảm
+                </div>
+              </div>
+              <div className='mt-8 flex items-center'>
+                <div className='capitalize text-gray-500'>Số lượng</div>
+                <div className='ml-10 flex items-center'>
+                  <button className='flex h-8 w-8 items-center justify-center rounded-l-sm border border-gray-300 text-gray-600'>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      strokeWidth='1.5'
+                      stroke='currentColor'
+                      className='h-4 w-4'
+                    >
+                      <path strokeLinecap='round' strokeLinejoin='round' d='M5 12h14' />
+                    </svg>
+                  </button>
+                  <InputNumber
+                    value={1}
+                    className=''
+                    classNameError='hidden'
+                    classNameInput='h-8 w-14 border-t border-b border-gray-300 p-1 text-center outline-none'
+                  />
+                  <button className='flex h-8 w-8 items-center justify-center rounded-l-sm border border-gray-300 text-gray-600'>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      strokeWidth='1.5'
+                      stroke='currentColor'
+                      className='h-4 w-4'
+                    >
+                      <path strokeLinecap='round' strokeLinejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
+                    </svg>
+                  </button>
+                </div>
+                <div className='ml-6 text-sm text-gray-500'>{product.quantity} sản phẩm có sẵn</div>
+              </div>
+              <div className='mt-8 flex items-center'>
+                <button className='flex h-12 items-center justify-center rounded-sm border-[#ee4d2d] bg-[#ee4d2d]/10 px-5 capitalize text-[#ee4d2d] shadow-sm hover:bg-[#ee4d2d]/5'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth='1.5'
+                    stroke='currentColor'
+                    className='mr-[10px] h-5 w-5 stroke-[#ee4d2d] text-[#ee4d2d]'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z'
+                    />
+                  </svg>
+                  Thêm vào giỏ hàng
+                </button>
+                <button className='ml-4 flex h-12 min-w-[5rem] items-center justify-center rounded-sm bg-[#ee4d2d] px-5 capitalize text-white shadow-sm outline-none hover:bg-[#ee4d2d]/90 '>
+                  Mua ngay
+                </button>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className='max-w-7xl mx-auto px-4'>
+        <div className='mt-8 bg-white p-4 shadow'>
+          <div className='rounded bg-gray-50 p-4 text-lg capitalize text-slate-700'>Mô tả sản phẩm</div>
+          <div className='mx-4 mt-12 mb-4 text-sm leading-loose'>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(product.description)
+              }}
+            />
           </div>
         </div>
       </div>
