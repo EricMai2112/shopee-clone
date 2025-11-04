f51e04cba3111985dc6699588b78af7eb196a233
EricMai2112 <mait58674@gmail.com>
2025-11-04 09:51:44 +0700

    divided into 2 sections and UI SideNav

--- src/pages/User/components/UserSideNav/UserSideNav.tsx
+++ src/pages/User/components/UserSideNav/UserSideNav.tsx
@@ -1,5 +1,65 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import path from 'src/constants/path'
 
 export default function UserSideNav() {
-  return <div>UserSideNav</div>
+  return (
+    <div>
+      <div className='flex items-center border-b border-b-gray-200 py-4'>
+        <Link to={path.profile} className='h-12 w-12 flex-shrink-0 overflow-hidden rounded-full border border-black'>
+          <img
+            src='https://down-vn.img.susercontent.com/file/vn-11134226-7r98o-ly98q0ssdzkda9_tn'
+            alt=''
+            className='h-full w-full object-cover'
+          />
+        </Link>
+        <div className='flex-grow pl-4'>
+          <div className='mb-1 truncate font-semibold text-gray-600'>congthanh</div>
+          <Link to={path.profile} className='flex items-center capitalize text-gray-500'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth='1.5'
+              stroke='currentColor'
+              className='size-5 mr-2'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10'
+              />
+            </svg>
+            Sửa hồ sơ
+          </Link>
+        </div>
+      </div>
+      <div className='mt-7'>
+        <Link to={path.profile} className='flex items-center capitalize text-[#ee4d2d] transition-colors'>
+          <div className='mr-3 flex h-[22px] w-[22px]'>
+            <img
+              src='https://down-vn.img.susercontent.com/file/ba61750a46794d8847c3f463c5e71cc4'
+              className='w-full h-full'
+            />
+          </div>
+          Tài khoản của tôi
+        </Link>
+        <Link to={path.changePassword} className='mt-4 flex items-center capitalize transition-colors'>
+          <div className='mr-3 flex h-[22px] w-[22px]'>
+            <img
+              src='https://down-vn.img.susercontent.com/file/ba61750a46794d8847c3f463c5e71cc4'
+              className='w-full h-full'
+            />
+          </div>
+          Đổi mật khẩu
+        </Link>
+        <Link to={path.historyPurchase} className='mt-4 flex items-center capitalize transition-colors'>
+          <div className='mr-3 flex h-[22px] w-[22px]'>
+            <img src='https://down-vn.img.susercontent.com/file/f0049e9df4e536bc3e7f140d071e9078' />
+          </div>
+          Đơn mua của tôi
+        </Link>
+      </div>
+    </div>
+  )
 }
